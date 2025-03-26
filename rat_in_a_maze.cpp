53ffed47c77a51367bd5df022226ab882c90c8ae
Sushant_Nair <94003837+zealsnair@users.noreply.github.com>
2025-03-26 15:45:20 +0530

    Update rat_in_a_maze.cpp

--- rat_in_a_maze.cpp
+++ rat_in_a_maze.cpp
@@ -1,59 +1,79 @@
-#include <bits/stdc++.h> 
-
-// TC = O(4 ^ (n^2)) 
-// SC = O(n*m) where n and m are refers to no of rows and columns
+#include <bits/stdc++.h>
+using namespace std;
 
 bool isSafe(int newX, int newY, vector<vector<int>> &arr, vector<vector<bool>> &visited, int n) {
-    if ((newX >=0 && newX < n) && (newY >= 0 && newY < n) && visited[newX][newY] != 1 && arr[newX][newY] != 0) {
+    if ((newX >= 0 && newX < n) && (newY >= 0 && newY < n) && !visited[newX][newY] && arr[newX][newY] != 0) {
         return true;
     }
     return false;
 }
 
-void solve(int x, int y, vector<string> &ans, string path, vector<vector<bool>> &visited, vector<vector<int >> &arr, int n ) {
-    if (x == n-1 && y == n-1) {
+void solve(int x, int y, vector<string> &ans, string path, vector<vector<bool>> &visited, vector<vector<int>> &arr, int n) {
+    if (x == n - 1 && y == n - 1) {
         ans.push_back(path);
-        return ;
+        return;
     }
 
-    // now we have four movements i,e, Down, Left, Right, Up
-
-    visited[x][y] = 1;
+    visited[x][y] = true;
 
     // Down
-    if (isSafe(x+1, y, arr, visited, n)) {
-        solve(x+1, y, ans, path+'D', visited, arr, n);
+    if (isSafe(x + 1, y, arr, visited, n)) {
+        solve(x + 1, y, ans, path + 'D', visited, arr, n);
     }
 
     // Left
-    if (isSafe(x, y-1, arr, visited, n)) {
-        solve(x, y-1, ans, path+'L', visited, arr, n);
+    if (isSafe(x, y - 1, arr, visited, n)) {
+        solve(x, y - 1, ans, path + 'L', visited, arr, n);
     }
 
-    // RIght
-    if (isSafe(x, y+1, arr, visited, n)) {
-        solve(x, y+1, ans, path+'R', visited, arr, n);
+    // Right
+    if (isSafe(x, y + 1, arr, visited, n)) {
+        solve(x, y + 1, ans, path + 'R', visited, arr, n);
     }
 
     // Up
-    if (isSafe(x-1, y, arr, visited, n)) {
-        solve(x-1, y, ans, path+'U', visited, arr, n);
+    if (isSafe(x - 1, y, arr, visited, n)) {
+        solve(x - 1, y, ans, path + 'U', visited, arr, n);
     }
 
-    visited[x][y] = 0;
+    visited[x][y] = false;
 }
 
-vector < string > searchMaze(vector < vector < int >> & arr, int n) {
-    // Write your code here.
+vector<string> searchMaze(vector<vector<int>> &arr, int n) {
     vector<string> ans;
-    if (arr[0][0] == 0) {
+    if (arr[0][0] == 0 || arr[n - 1][n - 1] == 0) {
         return ans;
     }
 
     string path = "";
-    vector<vector<bool>> visited(n, vector<bool> (n, 0));
+    vector<vector<bool>> visited(n, vector<bool>(n, false));
 
     solve(0, 0, ans, path, visited, arr, n);
-
     return ans;
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the maze (n x n): ";
+    cin >> n;
+
+    vector<vector<int>> maze(n, vector<int>(n));
+    cout << "Enter the maze (0 for blocked, 1 for open path):\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> maze[i][j];
+        }
+    }
+
+    vector<string> result = searchMaze(maze, n);
+    if (result.empty()) {
+        cout << "No path found!" << endl;
+    } else {
+        cout << "Paths found: " << endl;
+        for (const string &path : result) {
+            cout << path << endl;
+        }
+    }
+
+    return 0;
 }
